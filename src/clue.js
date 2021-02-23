da62e529c3a4d1f0bca576cae12f7574526e5b3f
Tran Van <vanbaotran201194@gmail.com>
2021-02-23 20:36:01 +0100

    info updated

--- src/clue.js
+++ src/clue.js
@@ -1,13 +1,41 @@
 // ITERATION 1
 
 // Suspects Collection
-const suspectsArray = [];
+const suspectsArray = [
+
+];
 
 // Rooms Collection
-const roomsArray = [];
+const roomsArray = [
+{rope, weight: 10},
+{knife, weight: 8},
+{candlestick, weight: 2},
+{poison, weight: 2},
+{dumbbell, weight: 30},
+{axe, weight: 15},
+{bat, weight: 13},
+{trophy, weight: 25},
+{pistol, weight: 20}
+];
 
 // Weapons Collection
-const weaponsArray = [];
+const weaponsArray = [
+Dining Room,
+Conservatory,
+Kitchen,
+Study,
+Library,
+Billiard Room,
+Lounge,
+Ballroom,
+Hall,
+Spa,
+Living Room,
+Observatory,
+Theater,
+Guest House,
+Patio
+];
 
 // ITERATION 2
 
